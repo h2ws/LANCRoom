59b41e0f81567ab058f5351e80a56e478f513083
oy_y <2846881204@qq.com>
2018-05-26 01:41:54 +0800

    初步实现表Dao

--- oyyD_Local_area_network_chat_room/server/com/oyyd/dao/ConnectDao.java
+++ oyyD_Local_area_network_chat_room/server/com/oyyd/dao/ConnectDao.java
@@ -2,7 +2,16 @@ package com.oyyd.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
+/**
+* Title: ConnectDao
+* Description: 
+* Version:1.0.0  
+* @author 28468
+* @date 2018年5月25日
+* 单例模式的连接操作
+*/
 public class ConnectDao {
 	static {
 
@@ -15,13 +24,21 @@ public class ConnectDao {
 		
 	}
 	private ConnectDao(){}
-	private Connection _conn;
-	public Connection getConn(String IP,int port) {
-		if (_conn.isClosed()) {
-			_conn = DriverManager.getConnection(String.format("jdbc:thin:@{0}:{1}8",IP,port),
+	
+	static final private String HOST = "127.0.0.1";
+	static final private int PORT = 8255;
+	
+	static private Connection _conn;
+	static public Connection getConn() throws SQLException {
+		if (_conn == null ||_conn.isClosed()) {
+			_conn = DriverManager.getConnection(String.format("jdbc:thin:@{0}:{1}8",HOST,PORT));
 		}
-		_conn.close();
 		return _conn;
+	}
+	static public void close() throws SQLException {
+		if (_conn != null && !_conn.isClosed()) {
+			_conn.close();
+		}
 	}
 	
 }
--- oyyD_Local_area_network_chat_room/server/com/oyyd/dao/UsersDao.java
+++ oyyD_Local_area_network_chat_room/server/com/oyyd/dao/UsersDao.java
@@ -1,5 +1,12 @@
 package com.oyyd.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
 * Title: UsersDao
@@ -9,14 +16,107 @@ package com.oyyd.dao;
 * @date 2018年5月23日
 */
 public class UsersDao {
+	
+	
+	/**
+	 * 获取all集合
+	 * @return
+	 * @throws SQLException
+	 */
+	static public Set<Entry> selAll() throws SQLException {
+		Set<Entry> ret = new HashSet<Entry>();
+		Connection conn = ConnectDao.getConn();
+		String sql = "select * from users";
+		Statement stm = conn.createStatement();
+		ResultSet resultset =  stm.executeQuery(sql);
+		while(resultset.next()) {
+			int uid = resultset.getInt("uid");
+			String username = resultset.getString("username");
+			String realname = resultset.getString("realname");
+			String password = resultset.getString("password");
+			String sign = resultset.getString("sign");
+			java.sql.Date rgdate = resultset.getDate("rgdate");
+			java.sql.Date birthdate = resultset.getDate("birthdate");
+			Entry entry = new Entry(uid, username, realname, password, sign, rgdate, birthdate);
+			ret.add(entry);
+			
+		}
+		
+		resultset.close();
+		stm.close();
+		conn.close();
+
+		return ret;
+	}
+	
+	/**
+	 * 单值查询,并返回
+	 * @param username
+	 * @param pwd
+	 * @return
+	 * @throws SQLException 
+	 */
+	static public Entry setOnly(String username, String pwd) throws SQLException{
+		Connection conn = ConnectDao.getConn();
+		String sql = "select * FROM where username = ? and password = ?";
+		PreparedStatement pre = conn.prepareStatement(sql);
+		pre.setString(1, username);
+		pre.setString(2, pwd);
+		ResultSet resultset = pre.executeQuery();
+		boolean only = resultset.next();
+		Entry entry = null;
+		if (only) {
+			int uid = resultset.getInt("uid");
+			String realname = resultset.getString("realname");
+			String sign = resultset.getString("sign");
+			java.sql.Date rgdate = resultset.getDate("rgdate");
+			java.sql.Date birthdate = resultset.getDate("birthdate");
+			entry = new Entry(uid, username, realname, pwd, sign, rgdate, birthdate);
+		}
+		
+		pre.close();
+		conn.close();
+		
+		return entry;
+	}
 
 	/**
+	 * 新增一个数据项，并返回对应实体
 	 * username重名则抛出异常
 	 * @param username
 	 * @param pwd
+	 * @throws SQLException 
 	 */
-	static public void register(String username, String pwd) {
+	static public Entry add(String username, String pwd) throws SQLException {
+		Connection conn = ConnectDao.getConn();
+		String sql = "insert into users (username, password) vlaue (?, ?)";
+		PreparedStatement pre = conn.prepareStatement(sql);
+		pre.setString(1, username);
+		pre.setString(2, pwd);
+		pre.executeUpdate();
+
+		pre.close();
+		conn.close();
 		
+		return new Entry(username, pwd);
+	}
+
+	/**
+	 * 从表中删除对应数据
+	 * @param user
+	 * @throws SQLException
+	 */
+	static public void del(Entry user) throws SQLException {
+		Connection conn = ConnectDao.getConn();
+		String sql = "delete users where uid = ?";
+		PreparedStatement pre = conn.prepareStatement(sql);
+		pre.setInt(1, user.getUid());
+		pre.executeUpdate();
+		
+		pre.close();
+		conn.close();
+		
+		user = null;
 	}
 	
 //	static public 
@@ -27,11 +127,30 @@ public class UsersDao {
 		private String realname ;
 		private String password;
 		private String sign;
-		private String rgdate;
-		private String birthdate;
+		private java.sql.Date rgdate;
+		private java.sql.Date birthdate;
 		
-		public Entry(String username) {
+		
+
+		public Entry(int uid, String username, String realname, String password, String sign, java.sql.Date rgdate,
+				java.sql.Date birthdate) {
+			super();
+			this.uid = uid;
 			this.username = username;
+			this.realname = realname;
+			this.password = password;
+			this.sign = sign;
+			this.rgdate = rgdate;
+			this.birthdate = birthdate;
+		}
+		
+		public int getUid() {
+			return uid;
+		}
+		
+		public Entry(String username, String pwd) {
+			this.username = username;
+			this.password = pwd;
 		}
 	}
 	
